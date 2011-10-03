faa64f65fc77e43cf0a28f343f35d7a1340b47ac
Scott Mason <darth_scottius@hotmail.com>
2011-10-02 19:10:36 -0700

    Initial try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-end
+
+
+    def authenticate(name,password)
+      user = User.find_by_username(name)
+      if user
+        if user.password == password
+          user
+        end
+      end
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
+
